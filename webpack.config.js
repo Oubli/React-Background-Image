2ccfcf61815bdb42a4ac0ab6aa2852736b4f6219
juanan <juanan@secmotic.com>
2019-08-31 04:52:38 +0200

    Added first version of webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/BackgroundImage.jsx',
+    output: {
+        path: path.resolve('lib'),
+        filename: 'BackgroundImage.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+}
